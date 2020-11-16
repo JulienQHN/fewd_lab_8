dc46a97bf12c635c4b1c755ddcdd9e764c4bc2ad
JulienQHN <julien.quennehen@edu.devinci.fr>
2020-11-16 23:59:49 +0000

    Push of fewd_lab_8

---
+++ index.js
@@ -0,0 +1,30 @@
+var Planets = ["Terre", "Mars", "Neptune","Uranus", "Saturne"];
+// Planets.forEach(planets => console.log(planets))
+
+ var a = 5; b = "Hello ", c = "World", d = 10;
+
+// console.log(a+d)
+
+var a = 5; b = 10; c = "5";
+var x = a;
+
+/* console.log(a == c);
+console.log(a === c);
+console.log(a == x);
+console.log(a != b);
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
+console.log(a >= c);
+console.log(a <= c); */
+
+var hello = function() {
+    return "hello wolrd";
+};
+var result = hello();
+
+console.log(hello);
+console.log(result);
+
+
